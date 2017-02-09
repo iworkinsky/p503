1ec2c7e67fa905aef5bc80a103e7a4fdbc83186d
iworkinsky <iworkinsky@gmail.com>
2017-02-09 16:25:33 +0800

    可以正常显示汉字，可以增加一条零件信息

--- p503/settings.py
+++ p503/settings.py
@@ -89,9 +89,13 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
